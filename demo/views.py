41837d02b4fe8edfbef051636f9e0daf91a752eb
myrmica-habilis <michalportes1@gmail.com>
2020-03-23 14:18:39 +0100

    Use the render shortcut in views, add minimal templates

--- demo/views.py
+++ demo/views.py
@@ -1,26 +1,23 @@
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('demo/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'demo/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse(f'You are looking at question {question_id}.')
+    return render(request, 'demo/detail.html', {'question_id': question_id})
 
 
 def results(request, question_id):
-    return HttpResponse('You are looking at the results of'
-                        f' question {question_id}.')
+    return render(request, 'demo/results.html', {'question_id': question_id})
 
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting on question {question_id}.')
+    return render(request, 'demo/vote.html', {'question_id': question_id})
