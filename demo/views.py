f3735efbbaf4fab813ee01ee6b7f8d65ad81245f
myrmica-habilis <michalportes1@gmail.com>
2020-03-23 14:25:18 +0100

    Raise Http404 for non-existent question

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -12,7 +13,11 @@ def index(request):
 
 
 def detail(request, question_id):
-    return render(request, 'demo/detail.html', {'question_id': question_id})
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'demo/detail.html', {'question': question})
 
 
 def results(request, question_id):
