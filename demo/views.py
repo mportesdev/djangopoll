43665f520976b0924932df4968a814cb568f4b04
myrmica-habilis <michalportes1@gmail.com>
2020-03-24 00:19:46 +0100

    Implement vote view and results template

--- demo/views.py
+++ demo/views.py
@@ -1,6 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
@@ -23,4 +25,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'demo/vote.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'demo/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice.',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('demo:results',
+                                            args=(question.id,)))
