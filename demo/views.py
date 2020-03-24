c3e1e08b8ecaee2551f75309cf99808c8d85bb69
myrmica-habilis <michalportes1@gmail.com>
2020-03-24 19:54:13 +0100

    Use generic views, expand db

--- demo/views.py
+++ demo/views.py
@@ -1,26 +1,28 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'demo/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'demo/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'demo/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'demo/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'demo/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'demo/results.html'
 
 
 def vote(request, question_id):
