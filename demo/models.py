13933c62511e2fa4c4c672397d641d65c5563292
myrmica-habilis <michalportes1@gmail.com>
2020-03-29 00:09:10 +0100

    Add year to the Choice model, migrate, update db

--- demo/models.py
+++ demo/models.py
@@ -27,7 +27,8 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    year = models.IntegerField(default=1900)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return f'{self.choice_text} ({self.year})'
